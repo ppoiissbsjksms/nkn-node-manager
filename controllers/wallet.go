4df45af15209cc950dc3da06ebb3a8d9f8e0a26d
bufrr <chouyc.adam@gmail.com>
2022-02-05 20:16:30 +0800

    bug fix

--- controllers/wallet.go
+++ controllers/wallet.go
@@ -83,12 +83,14 @@ func FindIdleWallet(c *gin.Context) {
 	// Get model if exist
 	var wallet models.Wallet
 	ts := time.Now().Unix()
-	err := models.DB.Where("idle = true AND last_update < ?", ts-3600).First(&wallet).Update(models.Wallet{Idle: false, IP: c.ClientIP()}).Error
+	err := models.DB.Where("idle = true AND last_update < ?", ts-3600).First(&wallet).Error
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-
+	wallet.Idle = false
+	wallet.IP = c.ClientIP()
+	models.DB.Save(&wallet)
 	c.JSON(http.StatusOK, gin.H{"idle": wallet})
 }
 
